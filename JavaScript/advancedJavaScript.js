63e902d54ab944f753c29816a8346617c643097e
satyendra pratap singh <ravisomvanshi9984@gmail.com>
2023-12-01 16:59:52 +0530

    jsRevision

---
+++ JavaScript/advancedJavaScript.js
@@ -0,0 +1,12 @@
+// Inside the computer's world there is only data.
+// Data is Every where.
+//Nubers and JS
+// JavaScript has a built-in Math methods which contains a 
+//small library of mathematical functions.
+var a = 10;
+console.log(a);
+var c=a;
+console.log(c,a);
+c=20;
+console.log(c,a);
+var b = { name:'masai'};  
