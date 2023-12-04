9630e19b80fac8d7978187030e1c2165f2ebae98
satyendra pratap singh <ravisomvanshi9984@gmail.com>
2023-12-04 22:25:30 +0530

    ReactP

--- mynewapp/src/Components/Form.js
+++ mynewapp/src/Components/Form.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 class Form extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+nameChangeHandler =(event)=>{
+  console.log(event.target.value)
+}
+  
   render() {
     return (
       <div>
         <form>
             <label htmlFor="" >Name</label>
-            <input type="text" name="name"  />
+            <input type="text" name="name" onKeyUp={this.nameChangeHandler} />
             <label htmlFor="">Country</label>
             <select name="" id="">
                 <option value="1">1</option>
