b4560dbdb7eba89556b02c775c0a6c3caf9d92d3
satyendra pratap singh <ravisomvanshi9984@gmail.com>
2023-12-09 03:52:00 +0530

    JavaScript REevise

--- JavaScript/advancedJavaScript.js
+++ JavaScript/advancedJavaScript.js
@@ -1,12 +1,57 @@
 // Inside the computer's world there is only data.
 // Data is Every where.
 //Nubers and JS
+
 // JavaScript has a built-in Math methods which contains a 
 //small library of mathematical functions.
-var a = 10;
-console.log(a);
-var c=a;
-console.log(c,a);
-c=20;
-console.log(c,a);
-var b = { name:'masai'};  
+
+// var a = 10;
+// console.log(a);
+// var c=a;
+// console.log(c,a);
+// c=20;
+// console.log(c,a);
+// var b = { name:'masai'}; 
+// You call 
+
+// what is variable
+// A container that stores that store the vaue.
+//it shows information
+//it can be premitive datatype /non-primitive
+// it can be small or big
+
+// var arr = [1,2,3];
+// console.log("arr",arr[0]);
+
+// arr vs arr[0]
+// arr- whole array
+// arr[0]- just showing an element
+
+// var str="Raju";
+// str[0]='k';
+// console.log("str", str);
+
+// Array element(non-primitive) can be changed ==> Mutable
+// String element(primitive) can not be changed ===> Immutable
+
+// How banks stores Wealth?
+
+// cash => Yes
+// gold => Yes
+// real State => No
+
+
+//JavaScript 
+// String--> Yes
+// number -> yes
+// objects, arrays-> yes
+
+
+//Two similar objects
+
+//Create two copies
+// 
+
+
+// primitives- points to the directly value;==>by value
+// non-primitives- it points to its state/address.==> by reference
