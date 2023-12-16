436ad93d70a22cc5b03371cc05e1a105057611e9
satyendra pratap singh <ravisomvanshi9984@gmail.com>
2023-12-17 01:57:45 +0530

    Weather app with location

--- JavaScript/weatherApp/index.js
+++ JavaScript/weatherApp/index.js
@@ -1,6 +1,6 @@
-
+//Apikey{af2a7c233c087af41c46b5640133a796}
 //https://api.openweathermap.org/data/2.5/weather?q=lucknow&appid=af2a7c233c087af41c46b5640133a796
-//
+//src="https://maps.google.com/maps?q=raebareli&t=&z=13&ie=UTF8&iwloc=&output=embed"
 
 
 
@@ -24,10 +24,27 @@ let getData = ()=>{
      
     // console.log("hello")
 }
+function getDataLocation(lat,lon){
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=af2a7c233c087af41c46b5640133a796 `
+    fetch(url)
+    .then(function(res){
+        // console.log(res);
+        return res.json();
+    })
+    .then(function(res){
+        console.log(res);
+        appendData(res);
+    })
+    .catch(function(err){
+        console.log(err)
+    });
+}
 
 // const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=af2a7c233c087af41c46b5640133a796`
 
 let appendData = (data)=>{
+    let map = document.getElementById("gmap_canvas");
+    
     let container = document.getElementById('container');
     container.innerHTML=null;
     let city = document.createElement('p');
@@ -48,4 +65,23 @@ let appendData = (data)=>{
     let humidity = document.createElement('p');
     humidity.innerText="HUMIDITY        : "+ data.main.humidity+"%";
     container.append(city,min,max,feel,humidity);
+    map.src= `https://maps.google.com/maps?q=${data.name}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+    container.classList.remove('hide');
+}
+
+
+
+
+function getWeather(){
+    navigator.geolocation.getCurrentPosition(success);
+    function success(position){
+        let crd = position.coords;
+        console.log("your current position is:");
+        console.log(`Latitute :${crd.latitude}`);
+        console.log(`longitude: ${crd.longitude}`);
+        console.log(`Mpre or less : ${crd.accuracy} maters.`)
+
+        getDataLocation(crd.latitude, crd.longitude);
+    }
+    
 }
